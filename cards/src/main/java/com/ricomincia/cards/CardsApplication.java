6c0fe9222493912eadd490a0067f34d34601fca0
Swathi <doranalaswathi97@gmail.com>
2023-10-08 13:46:52 +0530

    Cards Microservices Complete

---
+++ cards/src/main/java/com/ricomincia/cards/CardsApplication.java
@@ -0,0 +1,40 @@
+package com.ricomincia.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Swathi ",
+						email = "example@gmail.com",
+						url = "https://www.example.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.example.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Cards microservice REST API Documentation",
+				url = "http://localhost:8082/cards.html"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
