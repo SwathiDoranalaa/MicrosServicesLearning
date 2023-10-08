79cfd18155ce2669b76437e7edd7b0f5420d0d99
Swathi <doranalaswathi97@gmail.com>
2023-10-08 14:17:12 +0530

    Loans Microservices Complete

---
+++ loans/src/main/java/com/ricomincia/loans/LoansApplication.java
@@ -0,0 +1,40 @@
+package com.ricomincia.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "EazyBank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Swathi i",
+						email = "example@gmail.com",
+						url = "https://www.example.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.example.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Loans microservice REST API Documentation",
+				url = "https://localhost:8083/loans.html"
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
